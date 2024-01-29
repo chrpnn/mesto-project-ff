02af3c8ee3c73ccea330763dd34f5361c9847443
Alexander Cherepenin <chrpnn@yandex.ru>
2024-01-29 18:30:14 +0300

    refactor(index.js) функция удаления карточки, функция поиска шаблона, общие улучшения

--- scripts/index.js
+++ scripts/index.js
@@ -11,37 +11,42 @@
 const cardTemplate = document.getElementById("card-template");
 const placesList = document.querySelector(".places__list");
 
-function createCard(initialCards) {
-  // клонируем шаблон
-  const card = cardTemplate.content.cloneNode(true);
+// ф-ия поиска шаблона карточки
+function getTemplate() {
+  const cardTemplate = document.getElementById("card-template").content;
+  return cardTemplate.querySelector(".places__item");
+} 
 
+function createCard(cardData, deleteCard) {
+  const card = getTemplate().cloneNode(true);
   // заполняем данные из массива
-  card.querySelector(".card__title").textContent = initialCards.name;
-  card.querySelector(".card__image").src = initialCards.link;
-
+  card.querySelector(".card__title").textContent = cardData.name;
+  card.querySelector(".card__image").src = cardData.link;
+  card.querySelector(".card__image").alt = cardData.name;
   // выберем кнопку удаления
   const deleteButton = card.querySelector(".card__delete-button");
-
-  // Добавляем обработчик события для кнопки удаления
-  deleteButton.addEventListener("click", function () {
-    const cardItems = deleteButton.closest(".places__item");
-    cardItems.remove();
+  deleteButton.addEventListener("click", () => {
+    deleteCard(card); // Вызываем обработчик удаления карточки
   });
-
   return card;
+}
+
+// Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
 }
 
 function addCardsToPage(initialCards) {
   // Для каждой карточки в массиве
   initialCards.forEach(function (cardData) {
     // Создаем карточку
-    const card = createCard(cardData);
+    const card = createCard(cardData, deleteCard);
     // Добавляем карточку в список мест
     placesList.append(card);
   });
 }
 
 // вызываем функцию для добавления карточек на страницу при загрузке
-document.addEventListener("DOMContentLoaded", function () {
-  addCardsToPage(initialCards);
-});
+document.addEventListener("DOMContentLoaded", () => {
+    addCardsToPage(initialCards);
+  });
